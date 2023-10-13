acd66066bbbe6e32b9a5c83987dd5fe3812df321
yohanlee <145434043+Y0HANLEE@users.noreply.github.com>
2023-10-13 19:04:14 +0900

    MemberService

--- PROJECT_YOGA/src/main/java/org/zerock/service/MemberService.java
+++ PROJECT_YOGA/src/main/java/org/zerock/service/MemberService.java
@@ -2,9 +2,6 @@ package org.zerock.service;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.zerock.domain.BoardVO;
-import org.zerock.domain.Criteria;
 import org.zerock.domain.MemberVO;
 
 public interface MemberService {
@@ -12,8 +9,7 @@ public interface MemberService {
 	public List<MemberVO> getListMem();
 	public void insertMem(MemberVO member);
 	public MemberVO getMem(String userid);
-	public boolean deleteMem(String userid);
-	//public void quitMem(MemberVO vo);
+	public int deleteMem(String userid);
 	public int updateMem(MemberVO vo);
 	public int checkId(String userid);
 	public int updateAdmin(MemberVO vo);
